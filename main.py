d37c4549f08242c5c984072a0f501c38fd59234e
adamyen <adamyen214@hotmail.com>
2022-04-22 20:41:03 -0400

    [feat]: complete checkout

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import os
 import json
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
 import wiotp.sdk.application
+from dotenv import load_dotenv
+
+load_dotenv()
+HOST = os.getenv('HOST')
+print(f">>> Hosting: {HOST}:5000")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-prefix = 123
-payload = ["1","2", "3"]
 TOPICS = ["CustomerId"]
+# Opening JSON file
+f = open('products.json')
+product_price = json.load(f)
+sum = 0
+user_id = 0
 
 def mqtt_sub_callback(evt):
+    global user_id
     payload = json.dumps(evt.data).strip("{\" }").replace('"','').split(":")
     print(f">>> payload: {payload}")
     if payload[0] == TOPICS[0]:
@@ -20,23 +30,30 @@ def mqtt_sub_callback(evt):
         for topic in [user_scanned_item_topic, suggestion_topic]:
             print(topic)
             client.subscribeToDeviceEvents(eventId=topic)
+        socketio.send(f"con-{user_id}")
     else:
         prefix = 'item' if payload[1] == 'scanned_item' else 'sug'
-        print(f"{prefix}-{payload[2].lstrip(' ')}")
-        socketio.send(f"{prefix}-{payload[2].lstrip(' ')}")
+        item = payload[2].lstrip(' ')
+        print(f"{prefix}-{item}")
+        socketio.send(f"{prefix}-{item}")
+        if prefix == 'item':
+            send_total(item)
 
-def publish(client, topic, payload, qos = 2, retain = False):
-    result = client.publish(topic, payload, qos, retain)
-    status = result[0]
-    if status == 0:
-        print(f"Send to topic `{topic}`:    {payload}")
-    else:
-        print(f"Failed to send message to topic {topic}")
-
+def send_total(product):
+    global sum
+    print(f'>>> Price: {product_price[product]}')
+    sum += product_price[product]
+    sum = round(sum, 2)
+    socketio.send(f"sum-{sum}")  
 
 @socketio.on('connected')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
+
+@socketio.on('checkout')
+def handle_checkout():
+    result = client.publishEvent(typeId="RaspberryPi", deviceId="project", eventId=f"{user_id}:reset", msgFormat="json", data={'action': 'reset'}, qos = 2)
+    print(f"-----> Check out complete: {result}")
 
 @app.route("/")
 def hello_world():
@@ -56,4 +73,4 @@ client.deviceEventCallback = mqtt_sub_callback
 print('6')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host=HOST)
